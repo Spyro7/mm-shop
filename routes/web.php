59f28bbd267863b1d1a97e14371353febb65970d
Mirzabek Shirinov <jamshidovic@mail.ru>
2024-10-29 10:38:04 +0500

    pagecontroller, home, about, contact views and home template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/', [PageController::class, 'index'])->name('pages.index');
+Route::get('/about', [PageController::class, 'about'])->name('pages.about');
+Route::get('/contact', [PageController::class, 'contact'])->name('pages.contact');
