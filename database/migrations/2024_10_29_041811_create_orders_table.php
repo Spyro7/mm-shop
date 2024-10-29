3c9ad57cea1b70a2c9a76a7f25ae3b4819bd76dc
Mirzabek Shirinov <jamshidovic@mail.ru>
2024-10-29 09:28:38 +0500

    migrations orders and carts table

---
+++ database/migrations/2024_10_29_041811_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->decimal('total_price', 12, 2)->nullable();
+            $table->string('status')->default('pending');
+            $table->string('shipping_address')->nullable();
+            $table->string('payment_method')->nullable();
+            $table->string('payment_status')->default('pending');
+            $table->timestamp('ordered_at')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
