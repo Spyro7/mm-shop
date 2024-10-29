81f887a561cdb692c6f7c3647cd2347b1bf484df
Mirzabek Shirinov <jamshidovic@mail.ru>
2024-10-29 09:13:29 +0500

    migrations products brands categories table

---
+++ database/migrations/2024_10_28_080108_create_products_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->decimal('discounted_price', 10, 2)->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->integer('stock_quantity')->default(0);
+            $table->string('sku')->nullable();
+            $table->decimal('width', 8, 2)->nullable();
+            $table->decimal('height', 8, 2)->nullable();
+            $table->decimal('depth', 8, 2)->nullable();
+            $table->string('material')->nullable();
+            $table->string('color')->nullable();
+            $table->decimal('weight', 8, 2)->nullable();
+            $table->unsignedBigInteger('brand_id')->nullable();
+            $table->json('images')->nullable();
+            $table->decimal('rating', 3, 2)->default(0);
+            $table->integer('reviews')->default(0);
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
